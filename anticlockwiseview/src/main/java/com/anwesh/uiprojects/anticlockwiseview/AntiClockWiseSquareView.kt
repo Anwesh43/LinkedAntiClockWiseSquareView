25fa0e13f504d6494a2ac7bd50f03a99a8c3f71d
Anwesh43 <anweshthecool0@gmail.com>
2019-04-28 13:01:42 +0530

    adding AntiClockWiseSquare class

--- anticlockwiseview/src/main/java/com/anwesh/uiprojects/anticlockwiseview/AntiClockWiseSquareView.kt
+++ anticlockwiseview/src/main/java/com/anwesh/uiprojects/anticlockwiseview/AntiClockWiseSquareView.kt
@@ -177,4 +177,28 @@ class AntiClockWiseSquareView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class AntiClockWiseSquare(var i : Int) {
+
+        private val root : ACWSNode = ACWSNode(0)
+        private var curr : ACWSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
