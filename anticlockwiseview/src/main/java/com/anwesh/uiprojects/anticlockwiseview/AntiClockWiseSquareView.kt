e79d73ec9beb396bc28422cc43efe5086f65774b
Anwesh43 <anweshthecool0@gmail.com>
2019-04-28 11:24:41 +0530

    adding drawACWSNode extension method

--- anticlockwiseview/src/main/java/com/anwesh/uiprojects/anticlockwiseview/AntiClockWiseSquareView.kt
+++ anticlockwiseview/src/main/java/com/anwesh/uiprojects/anticlockwiseview/AntiClockWiseSquareView.kt
@@ -31,3 +31,40 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
+fun Canvas.drawAnticlockWiseLine(x : Float, y : Float, rot : Float, sc : Float, paint : Paint) {
+    save()
+    translate(-x, y)
+    rotate(-rot * sc)
+    drawLine(0f, 0f, 2 * x, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawACWSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val deg : Float = 360f / (lines + 1)
+    val rotDeg : Float = 180f - deg
+    val x : Float = size * Math.cos((rotDeg / 2) * Math.PI / 180).toFloat()
+    val y : Float = size * Math.sin((rotDeg / 2) * Math.PI / 180).toFloat()
+    var totalDeg : Float = 0f
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    drawLine(-x, y, 2 * x, y, paint)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        totalDeg += deg * Math.floor(sc.toDouble()).toFloat()
+        save()
+        rotate(totalDeg)
+        drawAnticlockWiseLine(-x, y, rotDeg, sc, paint)
+        restore()
+    }
+    restore()
+}
