9740fa116d78552f46c941a162f3dd9258a8a37d
Anwesh43 <anweshthecool0@gmail.com>
2019-04-28 12:59:24 +0530

    adding ACWSNode class

--- anticlockwiseview/src/main/java/com/anwesh/uiprojects/anticlockwiseview/AntiClockWiseSquareView.kt
+++ anticlockwiseview/src/main/java/com/anwesh/uiprojects/anticlockwiseview/AntiClockWiseSquareView.kt
@@ -133,4 +133,48 @@ class AntiClockWiseSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ACWSNode(var i : Int, val state : State = State()) {
+
+        private var next : ACWSNode? = null
+        private var prev : ACWSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ACWSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawACWSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ACWSNode {
+            var curr : ACWSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
